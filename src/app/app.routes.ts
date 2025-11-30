5d65e95ad98003cf506c872f27c41fba4397e133
Rachel Phan <py.rachel03@gmail.com>
2025-11-30 16:52:22 -0500

    Set up angular + routing for the screens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Signup } from './pages/signup/signup'; 
+import { Login } from './pages/login/login'; 
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/signup', pathMatch: 'full' }, 
+  { path: 'signup', component: Signup },
+  { path: 'login', component: Login },
+  { path: 'home', component: Home },
+];
